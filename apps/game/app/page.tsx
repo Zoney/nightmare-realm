0457bf577d72d00a750a90a35cae41aa8a108f20
Øyvind Brekkhus Sandåker <oyvind.sandaker@gmail.com>
2024-02-18 14:22:11 +0100

    two games

--- apps/game/app/page.tsx
+++ apps/game/app/page.tsx
@@ -3,8 +3,28 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Page() {
   return (
-    <main>
-      <div className="flex h-screen w-full flex-col items-center justify-center bg-[#bd1e59] p-4 text-center">
+    <main className="flex flex-col sm:flex-row bg-[#bd1e59]">
+      <div className="flex w-full flex-col items-center justify-center bg-[#bd1e59] p-4 text-center">
+        <Image
+          alt="Nightmare Realm Logo"
+          className="mb-8 max-w-xs md:max-w-lg"
+          src="/puppy1.png"
+          width={300}
+          height={300}
+        />
+
+        <h1 className="mb-4 text-4xl font-bold text-white">Puppy Jump</h1>
+        <Link
+          className="mb-4 w-64 bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800"
+          href={"puppy"}
+        >
+          Start Game
+        </Link>
+        <Button className="w-64 bg-white text-black" variant="outline">
+          About
+        </Button>
+      </div>
+      <div className="flex  w-full flex-col items-center justify-center bg-[#bd1e59] p-4 text-center">
         <Image
           alt="Nightmare Realm Logo"
           className="mb-8 max-w-xs md:max-w-lg"
@@ -14,7 +34,10 @@ export default function Page() {
         />
 
         <h1 className="mb-4 text-4xl font-bold text-white">Nightmare Realm</h1>
-        <Link className="mb-4 w-64 bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800" href={"puppy"}>
+        <Link
+          className="mb-4 w-64 bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800"
+          href={"/"}
+        >
           Start Game
         </Link>
         <Button className="w-64 bg-white text-black" variant="outline">
